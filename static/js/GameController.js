7f4cd1fadc32dedebe79a7bff0a40a52b54bcaba
hammadmlk <hammadmlk@gmail.com>
2015-06-09 20:29:09 -0700

    Controller with takeTurm, clickListener, and ...
    and clickCoordinate to matrix row,col conversion

--- static/js/GameController.js
+++ static/js/GameController.js
@@ -11,12 +11,52 @@ function GameController(canvas) {
 
 	this.create = function () {
 		self.GameModel = new GameModel(canvas);
-    self.GameView = new GameView(self.GameModel);
+		self.GameView = new GameView(self.GameModel);
 		self._initGame();
 	};
 
+	this._initGame = function () {
+		// Models and views are created, so we can start game here.
 
-	this._getRelativeCoords = function (event) {
+		self.GameView.draw();
+
+		//data binding by observing model
+		Object.observe(self.GameModel, function (changes) {
+			self.GameView.draw();
+		});
+
+		//click listener
+		self.GameModel.getCanvas().addEventListener('mousedown', function (evt) {
+			var coords = self._getRelativeCoordinates(evt);
+			var rowCol = self._getRowCol(coords.x, coords.y);
+      self._takeTurn(rowCol.row, rowCol.col);
+		}, false);
+
+	};
+  
+  this._takeTurn = function(row, col){
+    
+  };
+
+	this._getRowCol = function (xcoordinate, ycoordinate) {
+		if (xcoordinate <= 0) {
+			xcoordinate = 1;
+		};
+		if (ycoordinate <= 0) {
+			ycoordinate = 1;
+		};
+
+		var row = Math.ceil(xcoordinate / (self.GameModel.getCanvasWidth() / 3)) - 1;
+		var col = Math.ceil(ycoordinate / (self.GameModel.getCanvasHeight() / 3)) - 1; ;
+
+		return {
+			row : row,
+			col : col
+		};
+	};
+
+	//gets the x,y coordinates from event
+	this._getRelativeCoordinates = function (event) {
 		if (event.offsetX !== undefined && event.offsetY !== undefined) {
 			return {
 				x : event.offsetX,
@@ -27,25 +67,6 @@ function GameController(canvas) {
 			x : event.layerX,
 			y : event.layerY
 		};
-	};
-  
-
-	this._initGame = function () {
-		// Models and views are created, so we can start game here.
-    
-    self.GameView.draw();
-    
-		//data binding by observing model
-		Object.observe(self.GameModel, function (changes) {
-      self.GameView.draw();
-		});
-    
-    /*
-		self.gameModel.canvas.addEventListener('mousedown', function (evt) {
-			var coords = self._getRelativeCoords(evt);
-			self.onClick(coords.x, coords.y);
-		}, false)
-    */
 	};
 
 };
